668b3c8b05c685172735b029ae75a59682be9960
Andrii <dzura1996das@gmail.com>
2022-04-05 17:07:54 +0300

    added all files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { analyzeAndValidateNgModules } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { ShowButtonService } from './show-button.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `<button (click)="onClick()">Show date</button>
+    <h1 *ngIf="show">{{ date }}</h1>`,
+  providers: [ShowButtonService],
 })
-export class AppComponent {
-  title = 'button';
+export class AppComponent implements OnInit {
+  constructor(private service: ShowButtonService) {}
+
+  // WITHOUT SERVICE
+  // show: boolean = false;
+  // date: any = new Date();
+  // onClick() {
+  // this.show = !this.show;
+  // }
+
+  ///WITH SERVICE
+  date: any;
+  show: boolean = false;
+  ngOnInit(): void {
+    this.date = this.service.showDate();
+  }
+  onClick() {
+    this.show = !this.show;
+  }
 }
